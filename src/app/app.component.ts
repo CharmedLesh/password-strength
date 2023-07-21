bc2982e9a7579a2b7b7a4fcfec43d61d223339be
CharmedLesh <tur.lesh.5932@gmail.com>
2023-07-22 00:49:39 +0300

    visibility icon functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'password-strength';
+    inputType: 'text' | 'password' = 'password';
+
+    visibilityButtonClick() {
+        switch (this.inputType) {
+            case 'text':
+                this.inputType = 'password';
+                break;
+            case 'password':
+                this.inputType = 'text';
+                break;
+            default:
+                this.inputType = 'password';
+                break;
+        }
+    }
 }
